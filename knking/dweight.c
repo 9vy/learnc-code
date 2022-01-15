708813aa9aba913ee11ce2241fb4b8a2aaef849b
gustof chatami <eov@pm.me>
2022-01-15 21:19:32 +0700

    reducing variable

--- knking/dweight.c
+++ knking/dweight.c
@@ -1,18 +1,13 @@
-/* Computes the dimnesional weight of a 12" x 10" x 8" box */
+/* Computes the dimensional weight of a 12" x 10" x 8" box */
 
 #include <stdio.h>
 
 int main (void) {
-	int height, length, width, volume, weight;
-	
-	height = 8;
-	length = 12;
-	width = 10;
-	volume = height * length * width;
-	weight = (volume + 165) / 166;
+	int height = 8, length = 12, width = 10, weight;
+	weight = (height * length * width + 165) / 166;
 
 	printf("Dimensions: %dx%dx%d\n", length, width, height);
-	printf("Volume (cubic inches): %d\n", volume);
+	printf("Volume (cubic inches): %d\n", height * length * width);
 	printf("Dimensional weight (pounds): %d\n", weight);
 
 	return 0;
